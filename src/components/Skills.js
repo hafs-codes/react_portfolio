582f830efeda414230b96f9c414478fd0150bc2c
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:38:58 +0300

    feat: add skills section

---
+++ src/components/Skills.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Skills() {
+  return (
+<>
+<section id="Languages">
+  <h1 class="language-header">Skills</h1>
+  <div class="LanguagesFlex">
+  <div class="Item">
+    <img src="assets/images/php.jpg" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/java.png" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/mysql.png" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/python.png" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/laravel.png" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/htmlcss.png" alt="Framework"/>
+  </div>
+  <div class="Item">
+    <img src="assets/images/javascript.png" alt="Framework"/>
+  </div>
+  </div>
+</section>
+</>
+  )
+}
+
+export default Skills
