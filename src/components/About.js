e8ecc12ec06e73baeb2eb69f71daca9d0e34d0f7
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:25:02 +0300

    feat: add about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,27 @@ import React from 'react'
 
 function About() {
   return (
-    <div>
-      
+<>
+<section class="mainFlex" id="about">
+    <div class="flexContainer">
+      <div class="flexItem">
+        <h2>Hafsa Omar</h2>
+        <p>A passionate <span id="pink">Software developer</span> from <span id="pink">Nairobi</span></p>
+        <br/>
+        <br/>
+        <div id="arrow-container" onclick="scrollToProjects()">
+          <span id="arrowImage"></span>
+          <span class="arrow-content">
+          </span>
+          
+        </div>
+      </div>
+      <div class="flexItem">
+        <img src="assets/images/profile-pic.png" alt="Profile Picture" id="profile-img"/>
+      </div>
     </div>
+  </section>
+</>
   )
 }
 
