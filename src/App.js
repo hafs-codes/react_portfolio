76043864bb35304e55fcd716e010b7208fadf6e1
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 16:40:56 +0300

    fix: images slider

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <Header />
-      <About />
-      <Projects/>
-      <Skills/>
-      <Footer/>
-    </>
+<>
+<Header/>
+<About/>
+<Projects/>
+<Skills/>
+<Footer/>
+</>
   );
 }
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,32 +1,26 @@
-import React from 'react'
+import React from 'react';
+import ImageSlider from './ImageSlider';
+import LeftArrow from './LeftArrow';
+import RightArrow from './RightArrow';
+
 import fms from '../images/facultymanagementsystem.png';
 import admin from '../images/admin.png';
 import transaction from '../images/transaction.png';
 
-function Projects() {
-    return (
-        <>
-            <section id="Projects">
-                <span id="project-mini-header">Accomplished works⚒️</span>
-                <h2 class="project-header">Projects</h2>
-                <div class="projectsContainer">
-                    <div class="ProjectsFlex">
-                        <div class="projectItem">
-                            <img src="assets/images/facultymanagementsystem.png" alt="Framework"/>
-                        </div>
-                        <div class="projectItem">
-                            <img src="assets/images/admin.png" alt="Framework"/>
-                        </div>
-                        <div class="projectItem">
-                            <img src="assets/images/transaction.png" alt="Framework"/>
-                        </div>
-                    </div>
-                </div>
-                <div class="arrow" id="left-arrow">&#9664;</div>
-                <div class="arrow" id="right-arrow">&#9654;</div>
-            </section>
-        </>
-    )
-}
+const Projects = () => {
+  const images = [fms, transaction, admin];
 
-export default Projects
+  return (
+    <section id="Projects">
+      <span id="project-mini-header">Accomplished works⚒️</span>
+      <h2 className="project-header">Projects</h2>
+      <div className="projectsContainer">
+        <ImageSlider images={images} />
+      </div>
+      <LeftArrow />
+      <RightArrow />
+    </section>
+  );
+};
+
+export default Projects;
