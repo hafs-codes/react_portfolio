3ea085611553a8696a53c5268e748dba792fe018
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:33:08 +0300

    feat: add Projects to App

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import About from './components/About';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <>
       <Header />
       <About />
+      <Projects/>
     </>
   );
 }
