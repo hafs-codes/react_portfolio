ee212f4d31979a105b8311fe18c8da45eb4f6ea6
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 16:47:40 +0300

    fix: Header scroll

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,48 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-scroll';
 import myLogo from '../images/my-logo.png';
 
-function Header() {
+const Header = () => {
   return (
-<>
-<nav className="navbar">
-            <div className="logo">
-                <a>
-                    <img src={myLogo} className="logo-img" alt="My Logo" />
-                </a>
-            </div>
-            <ul className="nav-links">
-                <div className="menu">
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#Languages">Skills</a></li>
-                    <li><a href="#Projects">Projects</a></li>
-                    <li><a href="https://drive.google.com/file/d/1x7g6SLfgcZbLQ4woeQbMSsHe8z7u--tk/view?usp=sharing" target="_blank">Resume</a></li>
-                    <li><a href="#Contacts">Contact</a></li>
-                </div>
-            </ul>
-        </nav>
-</>
-  )
-}
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <a>
+            <img src={myLogo} className="logo-img" alt="My Logo" />
+          </a>
+        </div>
+        <ul className="nav-links">
+          <div className="menu">
+            <li>
+              <Link to="about" smooth={true} duration={100}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link to="Languages" smooth={true} duration={100}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link to="Projects" smooth={true} duration={100}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <a href="https://drive.google.com/file/d/1x7g6SLfgcZbLQ4woeQbMSsHe8z7u--tk/view?usp=sharing" target="_blank">
+                Resume
+              </a>
+            </li>
+            <li>
+              <Link to="Contacts" smooth={true} duration={100}>
+                Contact
+              </Link>
+            </li>
+          </div>
+        </ul>
+      </nav>
+    </>
+  );
+};
 
-export default Header
+export default Header;
