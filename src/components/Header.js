7643375b50e615d06d0f374eaa0d0c0b0bc4b101
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:15:22 +0300

    feat: add log Image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import myLogo from '../images/my-logo.png';
 
 export default function Header() {
-  return (
-    <nav className="navbar">
-    <div className="logo">
-      <a href="assets/images/my-logo.png">
-        <img src="assets/images/my-logo.png" className="logo-img" alt="My Logo"/>
-      </a>
-    </div>
-    <ul className="nav-links">
-      <div className="menu">
-        <li><a href="#about">About</a></li>
-        <li><a href="#Languages">Skills</a></li>
-        <li><a href="#Projects">Projects</a></li>
-        <li><a href="https://drive.google.com/file/d/1x7g6SLfgcZbLQ4woeQbMSsHe8z7u--tk/view?usp=sharing" target="_blank">Resume</a></li>
-        <li><a href="#Contacts">Contact</a></li>
-      </div>
-    </ul>
-  </nav>
-  )
+    return (
+        <nav className="navbar">
+            <div className="logo">
+                <a>
+                    <img src={myLogo} className="logo-img" alt="My Logo" />
+                </a>
+            </div>
+            <ul className="nav-links">
+                <div className="menu">
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#Languages">Skills</a></li>
+                    <li><a href="#Projects">Projects</a></li>
+                    <li><a href="https://drive.google.com/file/d/1x7g6SLfgcZbLQ4woeQbMSsHe8z7u--tk/view?usp=sharing" target="_blank">Resume</a></li>
+                    <li><a href="#Contacts">Contact</a></li>
+                </div>
+            </ul>
+        </nav>
+    )
 }
