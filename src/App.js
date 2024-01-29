c6592985d084be5c25112dbe1af5a4c52a61ae81
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:27:56 +0300

    feat: add About to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import About from './components/About';
 
 function App() {
   return (
-<Header/>
+    <>
+      <Header />
+      <About />
+    </>
   );
 }
 
