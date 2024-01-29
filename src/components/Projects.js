68156251cdfd072dd0fd343e2c876128ae03eaf1
Hafsa Omar <hafswa254@gmail.com>
2024-01-29 11:31:10 +0300

    feat: add projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,11 +1,29 @@
 import React from 'react'
 
 function Projects() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <>
+            <section id="Projects">
+                <span id="project-mini-header">Accomplished works⚒️</span>
+                <h2 class="project-header">Projects</h2>
+                <div class="projectsContainer">
+                    <div class="ProjectsFlex">
+                        <div class="projectItem">
+                            <img src="assets/images/facultymanagementsystem.png" alt="Framework"/>
+                        </div>
+                        <div class="projectItem">
+                            <img src="assets/images/admin.png" alt="Framework"/>
+                        </div>
+                        <div class="projectItem">
+                            <img src="assets/images/transaction.png" alt="Framework"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="arrow" id="left-arrow">&#9664;</div>
+                <div class="arrow" id="right-arrow">&#9654;</div>
+            </section>
+        </>
+    )
 }
 
 export default Projects
